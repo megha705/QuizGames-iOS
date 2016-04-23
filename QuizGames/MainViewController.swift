f244c820bda54934adcd6c5594b58fe6838694cb
radoslavyordanov <radoslav@kodovpotok.bg>
2016-04-23 14:13:32 +0300

    Added quiz games selection view.

---
+++ QuizGames/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  QuizGames
+//
+//  Created by Radoslav on 4/22/16.
+//  Copyright © 2016 Sourcestream. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var topScoresButton: UIButton!
+    @IBOutlet weak var aboutButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        playButton.layer.cornerRadius = 5
+        playButton.layer.borderWidth = 1
+        playButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        topScoresButton.layer.cornerRadius = 5
+        topScoresButton.layer.borderWidth = 1
+        topScoresButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        aboutButton.layer.cornerRadius = 5
+        aboutButton.layer.borderWidth = 1
+        aboutButton.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuizGames/QuizGamesSelectionViewController.swift
@@ -0,0 +1,38 @@
+//
+//  QuizGamesSelectionViewController.swift
+//  QuizGames
+//
+//  Created by Radoslav on 4/23/16.
+//  Copyright © 2016 Sourcestream. All rights reserved.
+//
+
+import UIKit
+
+class QuizGamesSelectionViewController: UIViewController {
+    
+    @IBOutlet weak var carsQuizButton: UIButton!
+    @IBOutlet weak var logosQuizButton: UIButton!
+    @IBOutlet weak var citiesQuizButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        carsQuizButton.layer.cornerRadius = 5
+        carsQuizButton.layer.borderWidth = 1
+        carsQuizButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        logosQuizButton.layer.cornerRadius = 5
+        logosQuizButton.layer.borderWidth = 1
+        logosQuizButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        citiesQuizButton.layer.cornerRadius = 5
+        citiesQuizButton.layer.borderWidth = 1
+        citiesQuizButton.layer.borderColor = UIColor.whiteColor().CGColor
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
