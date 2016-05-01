f0672f8a86eca96369638cc0706b781386e5100f
radoslavyordanov <radoslav@kodovpotok.bg>
2016-05-01 13:38:48 +0300

    Add logout confirm dialog.

---
+++ QuizGames/AddQuizViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AddQuizViewController.swift
+//  QuizGames
+//
+//  Created by Radoslav on 5/1/16.
+//  Copyright © 2016 Sourcestream. All rights reserved.
+//
+
+import UIKit
+
+class AddQuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- QuizGames/MainViewController.swift
+++ QuizGames/MainViewController.swift
@@ -12,6 +12,7 @@ class MainViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var topScoresButton: UIButton!
     @IBOutlet weak var aboutButton: UIButton!
+    @IBOutlet weak var addButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,20 +28,35 @@ class MainViewController: UIViewController {
         aboutButton.layer.cornerRadius = 5
         aboutButton.layer.borderWidth = 1
         aboutButton.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        if Util.userRoleId != 1 {
+            addButton.enabled = false
+            addButton.tintColor = UIColor.clearColor()
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     @IBAction func onLogoutTap(sender: AnyObject) {
-        let preferences = NSUserDefaults.standardUserDefaults()
-        preferences.setInteger(0, forKey: Util.USER_ID_PREF)
-        preferences.setInteger(0, forKey: Util.USER_ROLE_ID)
-        self.performSegueWithIdentifier("showLoginView", sender: sender)
+        let logoutAsk = NSLocalizedString("logoutAsk", comment: "")
+        let alert =  UIAlertController(title: nil, message: logoutAsk, preferredStyle: .Alert)
+        let yesAction: UIAlertAction = UIAlertAction(title: NSLocalizedString("yes", comment: ""), style: .Default) { action -> Void in
+            let preferences = NSUserDefaults.standardUserDefaults()
+            preferences.setInteger(0, forKey: Util.USER_ID_PREF)
+            preferences.setInteger(0, forKey: Util.USER_ROLE_ID)
+            self.performSegueWithIdentifier("showLoginView", sender: sender)
+        }
+        alert.addAction(yesAction)
+        let noAction: UIAlertAction = UIAlertAction(title: NSLocalizedString("no", comment: ""), style: .Default) { action -> Void in
+            
+        }
+        alert.addAction(noAction)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
-
+    
 }
 
